1a62586fe7e4f1090a49e558679776ca5ead7428
Renan Praxedes <c1329858@interno.bb.com.br>
2023-04-26 13:39:20 -0300

    webhook facebok

--- app.js
+++ app.js
@@ -12,9 +12,19 @@ app.get('/', (req, res) => {
 app.get('/api/info', (req, res) => {
   res.send({ application: 'sample-app', version: '1.0' });
 });
+
+
+app.get('/api/v1/webhook/facebook', (req, res) => {
+  res.send({ ...req.body });
+
+
+})
+
 app.post('/api/v1/getback', (req, res) => {
   res.send({ ...req.body });
 });
+
+
 app.listen(3000, () => console.log(`Listening on: 3000`));
 
 module.exports.handler = serverless(app);
